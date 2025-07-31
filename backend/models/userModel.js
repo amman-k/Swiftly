e0d64896cb603f80afd14b10f5f9bcb1d7d349aa
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-31 10:53:57 +0530

    created a User model

---
+++ backend/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+/**
+ * Mongoose schema for the User collection.
+ * This schema is designed for users authenticating via OAuth (e.g., Google).
+ * It stores the provider-specific ID instead of a local password.
+ */
+
+const userSchema=new mongoose.Schema({
+    googleId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    boards:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Board',
+    }]
+},{
+    timestamps:true,
+});
+
+const User=mongoose.model('User',userSchema);
+
+export default User;
