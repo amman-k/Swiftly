ad6b86e71bb5d22f49c98e3bdedc8a68f871e3ab
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-01 23:28:21 +0530

    added contoller to fetch and create new boards

---
+++ backend/controllers/boardController.js
@@ -0,0 +1,47 @@
+import Board from "../models/boardModel";
+import User from "../models/userModel";
+
+/**
+ * @desc    Get all boards for the logged-in user
+ * @route   GET /api/boards
+ * @access  Private
+ */
+const getBoards = async (req, res) => {
+  try {
+    const boards = await Board.find({ owner: req.user.id });
+    res.status(200).json(boards);
+  } catch (err) {
+    res.status(500).json({ message: "error fetching boards" });
+  }
+};
+
+/**
+ * @desc    Create a new board
+ * @route   POST /api/boards
+ * @access  Private
+ */
+
+const createBoard = async (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ message: "Title is required" });
+  }
+  try {
+    const newBoard = new Board({
+      title,
+      owner: req.user.id,
+    });
+    const savedBoard = await newBoard.save();
+
+    await User.findByIdAndUpdate(
+      req.user.id,
+      { $push: { boards: newBoard } },
+      { new: true }
+    );
+    res.status(201).json(savedBoard);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create new board" });
+  }
+};
+
+export { getBoards, createBoard };
