62ef00c37d844991f59708b78df4b62a0550dcf1
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-03 11:56:53 +0530

    wrapped components within authProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-
+import { AuthProvider } from "./context/AuthContext";
 
 const BoardsDashboard = () => (
   <h1 className="text-white text-3xl">Boards Dashboard</h1>
@@ -13,21 +13,21 @@ const LoginPage = () => <h1 className="text-white text-3xl">Login Page</h1>;
 
 function App() {
   return (
-    <div className="bg-[#2E3944] min-h-screen font-sans text-white">
-      <BrowserRouter>
-        <Routes>
-          {/* This now points to our full LandingPage.jsx component */}
-          <Route path="/" element={<LandingPage />} />
-          
-          <Route path="/boards" element={<BoardsDashboard />} />
-          <Route path="/board/:id" element={<BoardView />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <AuthProvider>
+      <div className="bg-[#2E3944] min-h-screen font-sans text-white">
+        <BrowserRouter>
+          <Routes>
+            {/* This now points to our full LandingPage.jsx component */}
+            <Route path="/" element={<LandingPage />} />
+
+            <Route path="/boards" element={<BoardsDashboard />} />
+            <Route path="/board/:id" element={<BoardView />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
-
-
 
 export default App;
