a1562d9900c0a39735aff6fc1b3d74b3e9e9f026
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-03 12:30:18 +0530

    added dashboard page to path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import { AuthProvider } from "./context/AuthContext";
+import BoardsDashboard from "./pages/BoardsDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const BoardsDashboard = () => (
-  <h1 className="text-white text-3xl">Boards Dashboard</h1>
-);
 const BoardView = () => (
   <h1 className="text-white text-3xl">Single Board View</h1>
 );
@@ -20,8 +19,8 @@ function App() {
             {/* This now points to our full LandingPage.jsx component */}
             <Route path="/" element={<LandingPage />} />
 
-            <Route path="/boards" element={<BoardsDashboard />} />
-            <Route path="/board/:id" element={<BoardView />} />
+            <Route path="/boards" element={<ProtectedRoute><BoardsDashboard /></ProtectedRoute>} />
+            <Route path="/board/:id" element={<ProtectedRoute><BoardView /></ProtectedRoute>} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>
         </BrowserRouter>
