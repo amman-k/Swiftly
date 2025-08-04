d2e37b56848933e40f9f4793651cd2f6fb2fbbaa
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-04 22:52:56 +0530

    fixed error which cause page to become unresponsive

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,50 +1,48 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
 
-export  const useAuth=()=>{
-    return useContext(AuthContext);
-}
 
-export const AuthProvider=({children})=>{
-    const [user,setUser]=useState(null);
-    const [loading,setLoading]=useState(null);
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
-    useEffect(()=>{
-        const checkLoggedIn =async ()=>{
-            try{
-                const {data}=await axios.get('http://localhost:5001/auth/current_user',{
-                    withCredentials:true,
-                });
 
-                if (data){
-                    setUser(data);
-                }
-            }catch(err){
-                console.error("Not authenticated",err);
-                setUser(null);
-            }finally{
-                setLoading(false)
-            }
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      try {
+      
+        const { data } = await axios.get('/auth/current_user');
+        
+        if (data) {
+          setUser(data);
         }
-        checkLoggedIn();
-    },[]);
-
-    const value={
-        user,
-        setUser,
-        loading,
+      } catch (error) {
+        console.error('Not authenticated', error.message);
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    return (
-        <AuthProvider value={value}>
-            {!loading && children}
-        </AuthProvider>
-    )
+    checkLoggedIn();
+  }, []); 
 
+  const value = {
+    user,
+    setUser,
+    loading,
+  };
 
+  return (
+    <AuthContext.Provider value={value}>
 
-
-}
-
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
