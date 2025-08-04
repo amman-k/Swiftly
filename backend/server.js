ac58300a4f85c9b92b4f1df4a0cf812cb86ddd22
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-04 22:56:32 +0530

    added mongo store for storing cookies

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import http from 'http';
-import {Server} from 'socket.io';
-import connectDB from './config/db.js';
+import { Server } from 'socket.io';
 import session from 'express-session';
-import passport  from 'passport';
+import passport from 'passport';
+import MongoStore from 'connect-mongo';
+import connectDB from './config/db.js';
 import configurePassport from './config/passport.js';
 import authRoutes from './routes/authRoutes.js';
 import boardRoutes from './routes/boardRoutes.js';
@@ -17,55 +18,68 @@ connectDB();
 
 configurePassport();
 
-const app=express();
-const PORT=process.env.PORT || 5000;
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
 
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true,
+    origin: 'http://localhost:5173',
+    credentials: true,
 }));
 app.use(express.json());
 
-if(!process.env.SESSION_SECRET){
+
+if (!process.env.SESSION_SECRET) {
     console.error("FATAL ERROR: SESSION_SECRET is not defined in the .env file.");
     process.exit(1);
 }
-
 app.use(
-    session({
-        secret:process.env.SESSION_TICKET,
-        resave:false,
-        saveUninitialized:false,
-    })
-)
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: 'sessions' 
+    }),
+    cookie: {
+      secure: 'auto',
+      httpOnly: true,
+      sameSite: 'lax', 
+      maxAge: 1000 * 60 * 60 * 24 
+    }
+  })
+);
+
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 const server = http.createServer(app);
-const io=new Server(server,{
-     cors: {
+const io = new Server(server, {
+    cors: {
         origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST"]
     }
 });
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log(`A user connected with socket id: ${socket.id}`);
     socket.on('disconnect', () => {
         console.log(`User with socket id ${socket.id} disconnected.`);
     });
 });
 
-app.use('/auth',authRoutes);
-app.use('/api/boards',boardRoutes);
-app.use('/api/lists',listRoutes);
+app.use('/auth', authRoutes);
+app.use('/api/boards', boardRoutes);
+app.use('/api/lists', listRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("Api is running Successfully");
+app.get('/', (req, res) => {
+    res.send('Swiftly API is running...');
 });
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
