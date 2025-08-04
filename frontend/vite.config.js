4945c1ce911e42b1eba30c3a7e40eb1c5415833c
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-04 22:51:02 +0530

    defined proxy routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+      '/auth': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+      }
+    }
+  }
 })
