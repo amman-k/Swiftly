fbd0c244d3a9aeb80dff8347fa13da9a1c6663ff
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-04 23:34:36 +0530

    added route to handle moved cards

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { createCard } from '../controllers/cardController.js';
+import { createCard, moveCard } from '../controllers/cardController.js';
 import {protect} from '../middlewares/authMiddleware.js';
 
 const router=new Router();
 
 router.route('/').post(protect,createCard);
+router.route('/:cardId/move').post(protect,moveCard);
 
 export default router;
 
