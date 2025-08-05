e6df27836bb5188ef6383c8a80f0a8dcd8681044
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-05 12:38:41 +0530

    added route for reordering of lists

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getBoards, createBoard, getBoardById } from "../controllers/boardController.js";
+import { getBoards, createBoard, getBoardById,reorderLists } from "../controllers/boardController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(protect, getBoards).post(protect, createBoard);
 router.route("/:id").get(protect,getBoardById);
+router.route('/:boardId/reorder-lists').put(protect, reorderLists);
 
 export default router;
