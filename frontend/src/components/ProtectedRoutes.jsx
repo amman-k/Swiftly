05b9dc9c278ace43a9240f65ac4e463777684090
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-06 18:56:14 +0530

    fixed uniterable issue

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { useAuth } from '../context/AuthContext'
+import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoutes = ({children}) => {
-    const [user,loading]=useAuth();
 
-    if (loading){
-        return null;
-    }
-    if(user){
-        return children;
-    }
-    return <Navigate to="/"></Navigate>
-}
+const ProtectedRoute = ({ children }) => {
+  
+  const { user, loading } = useAuth();
 
-export default ProtectedRoutes
+ 
+  if (loading) {
+    return null; 
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
