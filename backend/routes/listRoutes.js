e2e8570b281cce6464f082537ef5dfa920f866ab
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-06 19:51:12 +0530

    correctly updated put path from post path

--- backend/routes/listRoutes.js
+++ backend/routes/listRoutes.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import { createList, reorderCards } from '../controllers/listController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  createList,
+  reorderCards,
+  updateList,
+} from "../controllers/listController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.route('/').post(protect,createList);
-router.route('/:listId/reorder-cards').post(protect,reorderCards);
+router.route("/").post(protect, createList);
+router.route("/:listId").put(protect, updateList);
+router.route("/:listId/reorder-cards").put(protect, reorderCards);
 
 export default router;
-
-
-
