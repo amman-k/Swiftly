4b7173e6a9049765ea094eddf44d3f4a360ba257
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-07 11:39:36 +0530

    added the routes for card deletion

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
-import { createCard, moveCard } from '../controllers/cardController.js';
+import { createCard, moveCard,deleteCard } from '../controllers/cardController.js';
 import {protect} from '../middlewares/authMiddleware.js';
 
 const router=new Router();
 
 router.route('/').post(protect,createCard);
 router.route('/:cardId/move').put(protect,moveCard);
+router.route('/:cardId').delete(protect, deleteCard);
 
 export default router;
 
