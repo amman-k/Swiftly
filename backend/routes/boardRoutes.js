2f5edcb916742c8801d81c2f1d01461327839148
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-08 15:20:31 +0530

    added rooutes for updating board title

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,11 +1,22 @@
 import express from "express";
-import { getBoards, createBoard, getBoardById,reorderLists, deleteBoard } from "../controllers/boardController.js";
+import {
+  getBoards,
+  createBoard,
+  getBoardById,
+  reorderLists,
+  deleteBoard,
+  updateBoard,
+} from "../controllers/boardController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(protect, getBoards).post(protect, createBoard);
-router.route("/:id").get(protect,getBoardById).delete(protect,deleteBoard);
-router.route('/:boardId/reorder-lists').put(protect, reorderLists);
+router
+  .route("/:id")
+  .get(protect, getBoardById)
+  .delete(protect, deleteBoard)
+  .put(protect, updateBoard);
+router.route("/:boardId/reorder-lists").put(protect, reorderLists);
 
 export default router;
