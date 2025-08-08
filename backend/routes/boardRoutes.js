6a587a9782b1ccacae1bca89b3dd69d1f4250c5c
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-08 14:09:43 +0530

    added route to delete board

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getBoards, createBoard, getBoardById,reorderLists } from "../controllers/boardController.js";
+import { getBoards, createBoard, getBoardById,reorderLists, deleteBoard } from "../controllers/boardController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(protect, getBoards).post(protect, createBoard);
-router.route("/:id").get(protect,getBoardById);
+router.route("/:id").get(protect,getBoardById).delete(protect,deleteBoard);
 router.route('/:boardId/reorder-lists').put(protect, reorderLists);
 
 export default router;
