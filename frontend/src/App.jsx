18dd30e862e7336447db3db0569f9bff8aee44f5
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-08 23:50:34 +0530

    updated to use Toaster for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import { AuthProvider } from "./context/AuthContext";
 import BoardsDashboard from "./pages/BoardsDashboard";
 import ProtectedRoute from "./components/ProtectedRoutes";
 import BoardView from './pages/BoardView';
+import {Toaster} from 'react-hot-toast';
 
 const LoginPage = () => <h1 className="text-white text-3xl">Login Page</h1>;
 
 function App() {
   return (
     <AuthProvider>
+      <Toaster/>
       <div className="bg-[#2E3944] min-h-screen font-sans text-white">
         <BrowserRouter>
           <Routes>
