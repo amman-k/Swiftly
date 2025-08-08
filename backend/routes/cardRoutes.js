9bf57743fdba0c8c77352024f1683767af4cac7c
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-08-08 15:34:17 +0530

    added route to update card desc

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
-import { createCard, moveCard,deleteCard } from '../controllers/cardController.js';
+import { createCard, moveCard,deleteCard, updateCard } from '../controllers/cardController.js';
 import {protect} from '../middlewares/authMiddleware.js';
 
 const router=new Router();
 
 router.route('/').post(protect,createCard);
 router.route('/:cardId/move').put(protect,moveCard);
-router.route('/:cardId').delete(protect, deleteCard);
+router.route('/:cardId').delete(protect, deleteCard).put(protect,updateCard);
 
 export default router;
 
